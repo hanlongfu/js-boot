6bcab6bdd18889c0929b5d79a86d749c2a2d559c
hanlongfu <hfu@salemstate.edu>
2020-04-29 22:11:57 -0700

    oop update

--- oop.js
+++ oop.js
@@ -1,11 +1,9 @@
-/*
 let user = {
 	name: "jason mraz",
 	age: 26,
 	occupation: "singer",
 	"net worth": 10000000,
 };
-
 
 // console.log(user["age"]);
 
@@ -34,19 +32,19 @@ function upload(params) {
 //let {name, age, occupation} = user;
 // console.log(occupation);
 
-function hd(){
-  return {name: 'justin timberlake', age: 40};
+function hd() {
+	return { name: "justin timberlake", age: 40 };
 }
 //let {name, age} = hd();
 // console.log(name, age)
 
-function user({name, age}){
-  console.log(name, age);
+function user({ name, age }) {
+	console.log(name, age);
 }
-user({name: 'justin timberlake', age: 40});
+user({ name: "justin timberlake", age: 40 });
 
 //destructure Math object
-let {random} = Math;
+let { random } = Math;
 console.log(random);
 
 // 解构层级对象
@@ -69,11 +67,14 @@ let hd2 = {
 	name: "jason",
 	lesson: {
 		title: "javascript",
-  },
-  job: 'rock star'
+	},
+	job: "rock star",
 };
 
-let {name, lesson: {title, student=600}} = hd2;
+let {
+	name,
+	lesson: { title, student = 600 },
+} = hd2;
 console.log(title, student);
 
 function createElement(options) {
@@ -87,5 +88,185 @@ function createElement(options) {
 }
 createElement({ width: 600, height: 200, backgroundColor: "orchid" });
 
-*/
+//检测对象本身的属性
+let hd = {};
+hd.name = "后蹲人";
+hd.age = 60;
+hd.job = "programmer";
+delete hd.name;
+console.log(hd.hasOwnProperty("age")); // true
 
+//检测包含原型链上的属性
+console.log(hd);
+console.log("toString" in hd);
+
+//设置对象的原型
+let a = {
+	name: "google",
+};
+let b = {
+	url: "google.com",
+};
+Object.setPrototypeOf(a, b);
+console.log(a);
+
+let shopping = {
+	user: "jason",
+	item: 4,
+	price: 50,
+};
+//检测对象的属性 Object.getOwnPropertyDescriptor(obj, "property")
+// console.log(
+// 	JSON.stringify(Object.getOwnPropertyDescriptor(shopping, "user"), null, 2)
+// );
+
+Object.defineProperty(shopping, "user", {
+	value: "mraz",
+	writable: false,
+	enumerable: true,
+	configurable: true,
+});
+
+// console.log(
+// 	JSON.stringify(Object.getOwnPropertyDescriptors(shopping), null, 2)
+// );
+
+Object.defineProperties(shopping, {
+	user: {
+		value: "michael",
+		writable: false,
+		enumerable: false,
+		configurable: true,
+	},
+	item: {
+		value: 50,
+		writable: false,
+		enumerable: true,
+		configurable: true,
+	},
+	price: {
+		value: 100,
+		writable: false,
+		enumerable: true,
+		configurable: true,
+	},
+});
+
+let shopping = {
+	user: "jason",
+	item: 4,
+	price: 50,
+};
+
+//Object.preventExtensions(shopping);
+shopping.name = "mraz";
+//console.log(shopping);
+
+if (Object.isExtensible(shopping)) {
+	console.log("hello world");
+}
+
+//封闭对象  - 不可以添加属性或者删除
+Object.seal(shopping);
+console.log(
+	JSON.stringify(Object.getOwnPropertyDescriptor(shopping, "user"), null, 2)
+);
+//不可以添加，也不可以删除
+shopping.extra = "lily hunter";
+delete shopping.price;
+
+let shopping = {
+	user: "jason",
+	item: 4,
+	price: 50,
+};
+
+Object.freeze();
+console.log(
+	JSON.stringify(Object.getOwnPropertyDescriptor(shopping, "user"), null, 2)
+);
+shopping.name = "michael";
+
+//使用访问器
+const user = {
+	data: { name: "houdunren", age: 10 },
+	set age(value) {
+		if (typeof value != "number" || value < 10 || value > 100) {
+			throw new Error("Invalid age");
+		}
+	},
+	get age() {
+		return this.data.age;
+	},
+};
+// user.age = 999;  //Invalid age
+// console.log(user.age);
+
+//访问器设置token为本地存储，给人感觉是从对象中获取
+let request = {
+	set token(content) {
+		localStorage.setItem("token", content);
+	},
+	get token() {
+		let token = localStorage.getItem("token", content);
+		if (!token) alert("please sign in");
+		return token;
+	},
+};
+Request.token = "3456jsbb67ijkxh12B3";
+console.log(Request.token);
+
+//代理拦截对象
+const hd = { name: "houdunren" };
+const proxy = new Proxy(hd, {
+	get(obj, property) {
+		return obj[property];
+	},
+	set(obj, property, value) {
+		obj[property] = value;
+		return true;
+	},
+});
+console.log(proxy.name);
+proxy.name = "向军";
+console.log(proxy);
+
+//代理拦截函数操作
+function factorial(num) {
+	return num == 1 ? 1 : num * factorial(num - 1);
+}
+let proxy = new Proxy(factorial, {
+	apply(func, obj, args) {
+		console.time("run");
+		func.apply(this, args);
+		console.timeEnd("run");
+	},
+});
+proxy.apply({}, [5]);
+
+//代理拦截数组
+let lessons = [
+	{
+		title: "mysql breakthrough",
+		category: "mysql",
+	},
+	{
+		title: "learn flex in 3 days",
+		category: "css",
+	},
+	{
+		title: "react app made easy",
+		category: "react",
+	},
+];
+
+let proxy = new Proxy(lessons, {
+	get(array, key) {
+		const title = array[key].title;
+		const len = 5;
+		array[key].title =
+			title.length > len ? title.substr(0, len) + ".".repeat(3) : title;
+		return array[key];
+	},
+});
+console.log(proxy[1]);
