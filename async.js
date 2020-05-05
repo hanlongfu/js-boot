8e9e5409c57b1bd5c8f50a1d566f17ed7cde83e1
hanlongfu <hfu@salemstate.edu>
2020-05-04 23:49:54 -0700

    promise#1

--- async.js
+++ async.js
@@ -8,10 +8,34 @@ const getIds = new Promise((resolve, reject) => {
 	}, 1500);
 });
 
+const getRecipe = (recId) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(
+			(Id) => {
+				const recipe = {
+					title: "Fresh Tomato Pasta",
+					publisher: "McGraw Hill",
+				};
+				resolve(`${Id}: ${recipe.title}`);
+			},
+			1500,
+			recId
+		);
+	});
+};
+
 //consume promise
 //.then is a handler function and inside it a callback function
 //that will execute when the promise is successful.
 //the argument - "Ids" - is the result of the successful promise
-getIds.then((Ids) => {
-	console.log(Ids);        
-});
+getIds
+	.then((Ids) => {
+		console.log(Ids);
+		return getRecipe(Ids[2]); // return 456: Fresh Tomato Pasta
+	})
+	.then((recipe) => {
+		console.log(recipe); //this logs the above result
+	})
+	.catch((error) => {
+		console.log("Error");
+	});
