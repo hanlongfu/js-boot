62d58450134d8e41e392c8a4c9d088324c6ecea8
hanlongfu <hfu@salemstate.edu>
2020-05-05 23:17:28 -0700

    fetch#2

--- async.js
+++ async.js
@@ -71,15 +71,20 @@ const getRelated = (publisher) => {
 // 	return related;
 // }
 // getRecipesAW().then((recipe) => console.log(recipe));
-
-fetch(
-	"https://crossorigin.me/https://www.metaweather.com/api/location/search/?lattlong=50.068,-5.316"
-)
-	.then((result) => {
-		console.log(result);``
-		return result.json();
-	})
-	.then((data) => {
-		console.log(data);
-	})
-	.catch((error) => console.log(error));
+function getWeather(woeid) {
+	fetch(
+		"https://crossorigin.me/https://www.metaweather.com/api/location/${woeid}/"
+	)
+		.then((result) => {
+			console.log(result);
+			return result.json();
+		})
+		.then((data) => {
+			const today = data.consolidated_weather[0];
+			console.log(
+				`Temperatures in ${data.title} stay between ${today.min_temp} and ${today.max_temp}.`
+			);
+		})
+		.catch((error) => console.log(error));
+}
+getWeather(2487956);
